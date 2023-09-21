bdfe53815072c85dd09a6020316f954b6d49d98a
HuilingLizzu <145653779+HuilingLizzu@users.noreply.github.com>
2023-09-21 20:24:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 // JavaScript code in the script file
 
-// Dynamically add major publications
+// Dynamically add education background
+const educationList = document.getElementById('education-list');
+const educationData = [
+    { date: 'Sep 2021 -- Present', degree: 'Master of Engineering', school: 'Zhengzhou University', location: 'Zhengzhou, China', major: 'Computer Science and Technology', gpa: '3.91/4.3' },
+    { date: 'Sep 2017 -- Jul 2021', degree: 'Bachelor of Engineering', school: 'Zhengzhou University', location: 'Zhengzhou, China', major: 'Software Engineering (Outstanding Engineer Program, Ministry of Education)', gpa: '3.61/4.0' },
+    // Add more education background data
+];
+
+educationData.forEach(item => {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `
+        <strong>${item.date}</strong> - ${item.degree}<br>
+        ${item.school}, ${item.location}<br>
+        Major: ${item.major}<br>
+        Cumulative GPA: <strong>${item.gpa}</strong>
+    `;
+    educationList.appendChild(listItem);
+});
+
+// Dynamically add major publications (excluding "Minor Revision" status)
 const publicationsList = document.getElementById('publications-list');
 const publicationsData = [
     {
@@ -30,25 +49,4 @@ const publicationsData = [
         pages: "4921-4925"
     },
     {
-        title: "Human-in-the-loop Real-time Task Allocation",
-        authors: "Huiling Li, Lei Gao, Hua Wang, Mingliang Xu, Yafei Li",
-        conference: "Proceedings of the IEEE Conference on Mobile Data Management (MDM), MUST Workshop",
-        year: "2022",
-        pages: "518-523"
-    }
-    // Add more publication data
-];
-
-publicationsData.forEach(item => {
-    const listItem = document.createElement('li');
-    listItem.innerHTML = `
-        <strong>${item.title}</strong><br>
-        Authors: ${item.authors}<br>
-        ${item.journal ? `Journal: ${item.journal}<br>` : ''}
-        ${item.conference ? `Conference: ${item.conference}<br>` : ''}
-        ${item.pages ? `Pages: ${item.pages}<br>` : ''}
-        Year: ${item.year}<br>
-        }
-    `;
-    publicationsList.appendChild(listItem);
-});
+        title: "Human-in-the-loop Real-time Task
