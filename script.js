5de070059d678830ebe97efc583bdf24347247fc
HuilingLizzu <145653779+HuilingLizzu@users.noreply.github.com>
2023-09-21 20:36:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 // Dynamically add education background
 const educationList = document.getElementById('education-list');
 const educationData = [
-    { date: 'Sep 2021 -- Present', degree: 'Master of Engineering', school: 'Zhengzhou University', location: 'Zhengzhou, China', major: 'Computer Science and Technology', gpa: '3.91/4.3' },
-    { date: 'Sep 2017 -- Jul 2021', degree: 'Bachelor of Engineering', school: 'Zhengzhou University', location: 'Zhengzhou, China', major: 'Software Engineering (Outstanding Engineer Program, Ministry of Education)', gpa: '3.61/4.0' },
+    { date: 'Sep 2021 -- Present', degree: 'Master of Engineering', school: 'Zhengzhou University', location: 'Zhengzhou, China', major: 'Computer Science and Technology' },
+    { date: 'Sep 2017 -- Jul 2021', degree: 'Bachelor of Engineering', school: 'Zhengzhou University', location: 'Zhengzhou, China', major: 'Software Engineering (Outstanding Engineer Program, Ministry of Education)' },
     // Add more education background data
 ];
 
@@ -13,8 +13,7 @@ educationData.forEach(item => {
     listItem.innerHTML = `
         <strong>${item.date}</strong> - ${item.degree}<br>
         ${item.school}, ${item.location}<br>
-        Major: ${item.major}<br>
-        Cumulative GPA: <strong>${item.gpa}</strong>
+        Major: ${item.major}
     `;
     educationList.appendChild(listItem);
 });
@@ -24,33 +23,33 @@ const publicationsList = document.getElementById('publications-list');
 const publicationsData = [
     {
         title: "Utility-Aware Dynamic Ridesharing in Spatial Crowdsourcing",
-        authors: "Yafei Li, Huiling Li, Xin Huang, Jianliang Xu, Yu Han, Mingliang Xu",
+        authors: "Yafei Li, <strong>Huiling Li</strong>, Xin Huang, Jianliang Xu, Yu Han, Mingliang Xu",
         journal: "IEEE Transactions on Mobile Computing (TMC)",
         year: "2022"
     },
     {
         title: "Fairness-Guaranteed Task Assignment for Crowdsourced Mobility Services",
-        authors: "Yafei Li, Huiling Li, Baolong Mei, Xin Huang, Jianliang Xu, Mingliang Xu",
+        authors: "Yafei Li, <strong>Huiling Li</strong>, Baolong Mei, Xin Huang, Jianliang Xu, Mingliang Xu",
         journal: "IEEE Transactions on Mobile Computing (TMC)",
         year: "2023"
     },
     {
         title: "AMRAS: A Visual Analysis System for Spatial Crowdsourcing",
-        authors: "Qingshun Wu, Yafei Li, Huiling Li, Di Zhang, Guanglei Zhu",
+        authors: "Qingshun Wu, Yafei Li, <strong>Huiling Li</strong>, Di Zhang, Guanglei Zhu",
         journal: "Proceedings of the VLDB Endowment (PVLDB), Demo Track",
         year: "2022",
         pages: "15(12): 3690-3693"
     },
     {
         title: "PRID: An Efficient Pub/Sub Ride Hitching System",
-        authors: "Yafei Li, Lei Gao, Haobo Sun, Huiling Li, Qingshun Wu",
+        authors: "Yafei Li, Lei Gao, Haobo Sun, <strong>Huiling Li</strong>, Qingshun Wu",
         conference: "Proceedings of the ACM Conference on Information and Knowledge Management (CIKM), Demo Track",
         year: "2022",
         pages: "4921-4925"
     },
     {
         title: "Human-in-the-loop Real-time Task Allocation",
-        authors: "Huiling Li, Lei Gao, Hua Wang, Mingliang Xu, Yafei Li",
+        authors: "<strong>Huiling Li</strong>, Lei Gao, Hua Wang, Mingliang Xu, Yafei Li",
         conference: "Proceedings of the IEEE Conference on Mobile Data Management (MDM), MUST Workshop",
         year: "2022",
         pages: "518-523"
