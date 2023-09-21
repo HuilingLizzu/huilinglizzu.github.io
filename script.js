993afb790fe2dab3b3c7a12c24338b3fd72b064a
HuilingLizzu <145653779+HuilingLizzu@users.noreply.github.com>
2023-09-21 19:40:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+/* 样式文件中的样式 */
+.main {
+    /* 添加整体页面样式 */
+    font-family: Arial, sans-serif;
+    padding: 20px;
+}
+
+.profile {
+    display: flex;
+    align-items: center;
+}
+
+.profile img {
+    width: 150px;
+    height: 150px;
+    border-radius: 50%;
+    margin-right: 20px;
+    object-fit: cover;
+}
+
+.bio {
+    flex: 1;
+}
+
+.education {
+    margin-top: 20px;
+}
+
+.education h3 {
+    font-size: 1.2rem;
+    margin-bottom: 10px;
+}
+
+/* 可根据需要继续自定义样式 */
 
